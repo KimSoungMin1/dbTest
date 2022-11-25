b0870b4d49e6bc5a1a82309ad9c0bcbba9412f8b
KimSoungMin1 <tjdals247470@naver.com>
2022-11-25 17:45:46 +0900

    구현중

--- db-react/src/database/firebase.js
+++ db-react/src/database/firebase.js
@@ -1,14 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore/lite";
-const firebaseConfig = {
-  apiKey: "AIzaSyBm7XU9rV_OKy9QI0No0gthPpwVN6giVX8",
-  authDomain: "test-firebase-3808c.firebaseapp.com",
-  projectId: "test-firebase-3808c",
-  storageBucket: "test-firebase-3808c.appspot.com",
-  messagingSenderId: "1048575199670",
-  appId: "1:1048575199670:web:384846ba43922e6dd75917",
-  measurementId: "G-4G688RNRQN",
-};
+const firebaseConfig = {};
 
 const firebaseApp = initializeApp(firebaseConfig);
 const database = getFirestore(firebaseApp); //정보가 올바르면 아래 파이어스토어 접근c
